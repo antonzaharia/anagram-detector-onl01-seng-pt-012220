48b6f07b30473f7e828b45547784c0e5085bf30e
ANTON ZAHARIA <zaharia_anton@yahoo.com>
2020-02-09 19:53:00 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,22 @@
-# Your code goes here!
+class Anagram
+  attr_accessor :word
+  
+  def initialize(word)
+    @word = word
+  end
+  
+  def match(array)
+    w_word = @word.split("")
+  array.map do |word|
+    to_return = []
+    a_word = word.split("")
+    if a_word.sort == w_word.sort
+      to_return << word
+      return to_return
+    else
+      to_return.clear
+      return to_return
+    end
+  end
+end
+end
